6f67e720277d5ad0dd8824e9bb61c80df7cafa98
Tyler Tedeschi <tjtedeschi@gmail.com>
2018-04-01 14:17:55 -0700

    Complete A+B with problem description added

--- Python/1000s/1000-1010/A+B-1000.py
+++ Python/1000s/1000-1010/A+B-1000.py
@@ -1,3 +1,18 @@
+/*
+Problem: http://poj.org/problem?id=1000
+Description
+Calculate a+b 
+Input
+Two integer a,b (0<=a,b<=10)
+Output
+Output a+b
+Sample Input
+1 2
+Sample Output
+3
+*/
+
+
 cmdin = input()
 integers = cmdin.split(' ')
 summation = 0
